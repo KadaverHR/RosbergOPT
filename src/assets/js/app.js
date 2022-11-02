823e2c9f0e42b3a965ac1fb07dd023affc8cd60f
Кондрашова Наталия Владимировна <ich@hasslich.ru>
2022-11-02 17:24:32 +0300

    sdf

---
+++ src/assets/js/app.js
@@ -0,0 +1,49 @@
+window.onload = function () {
+
+
+  const Svg =()=>{
+    let x = [".svg"];
+    x.forEach(item => {
+      $(item).each(function() {
+        let $img = $(this);
+        let imgClass = $img.attr("class");
+        let imgURL = $img.attr("src");
+        $.get(imgURL, function(data) {
+          let $svg = $(data).find("svg");
+          if (typeof imgClass !== "undefined") {
+            $svg = $svg.attr("class", imgClass + " replaced-svg");
+          }
+          $svg = $svg.removeAttr("xmlns:a");
+          if (!$svg.attr("viewBox") && $svg.attr("height") && $svg.attr("width")) {
+            $svg.attr("viewBox", "0 0 " + $svg.attr("height") + " " + $svg.attr("width"));
+          }
+          $img.replaceWith($svg);
+        }, "");
+      });
+    });
+  }
+  // Svg()
+
+
+
+  let burger = document.querySelector('.burger');
+  let menu = document.querySelector('.header__list-mobile');
+  let menuLinks = menu.querySelectorAll('.header__link');
+
+
+  burger.addEventListener('click', function () {
+      burger.classList.toggle('burger--active');
+      menu.classList.toggle('header__list-mobile--active');
+      document.body.classList.toggle('stop-scroll');
+  });
+
+  menuLinks.forEach(function (el) {
+      el.addEventListener('click', function () {
+          burger.classList.remove('burger--active');
+          menu.classList.remove('header__list-mobile--active');
+          document.body.classList.remove('stop-scroll')
+      })
+  });
+
+
+}
